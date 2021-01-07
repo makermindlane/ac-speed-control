ab6895bacd2a8abd7480e4913129c18d2cd2937d
Pawan <makermindlane@gmail.com>
2021-01-07 21:14:54 +0530

    initial commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,71 +1,55 @@
 #include <Arduino.h>
 
-const int DIAC_PIN = 3;
+const uint8_t DIAC_PIN = 3;
 
-int myDelay = 0;
-int myvalue = 0;
-int last_CH1_state = 0;
+bool enablePhaseControl = false;
+int speedVal = 0;
+uint8_t prevStatus = 0;
 
-void setup() {
-  /*
-   * Port registers allow for lower-level and 
-   * faster manipulation of the i/o pins of the microcontroller on an Arduino board. 
-   * The chips used on the Arduino board 
-   * (the ATmega8 and ATmega168) have three ports:
-     -B (digital pin 8 to 13)
-     -C (analog input pins)
-     -D (digital pins 0 to 7)
-  //All Arduino (Atmega) digital pins are inputs when you begin
-  */
-  
+void setup() { 
   //enable PCMSK0 scan
   PCICR |= (1 << PCIE0);
-  //Set pin D8 trigger an interrupt on state change. Input from optocoupler
+  //Set pin D8 trigger an interrupt on state change.
   PCMSK0 |= (1 << PCINT0);
-  //Define D3 as output for the DIAC pulse
   pinMode(DIAC_PIN, OUTPUT);
-  //Start serial com with the BT module (RX and TX pins)
+  // Setting baud rate to 9600 for serial communication from bluetooth module.
   Serial.begin(9600); 
 }
 
 void loop() {
-   /*Read the speed value via Bluetooth and map it from 10 to 10.000 us.
+   /*Read the speed value via Bluetooth and map it from 10 to 10000 us.
      AC frequency is 50Hz, so period is 20ms. We want to control the power
-     of each half period, so the maximum is 10ms or 10.000us.
+     of each half period, so the maximum is 10ms or 10000us.
    */
    if(Serial.available() > 0) { 
-      myvalue = map(Serial.read(), 0, 255, 10000, 10);
+      speedVal = map(Serial.read(), 0, 255, 10000, 10);
    }
-    if (myDelay) {
+    if (enablePhaseControl) {
       //This delay controls the power
-      delayMicroseconds(myvalue); 
+      delayMicroseconds(speedVal); 
       digitalWrite(DIAC_PIN, HIGH);
       delayMicroseconds(100);
       digitalWrite(DIAC_PIN, LOW);
-      myDelay = 0;
+      enablePhaseControl = 0;
     } 
 }
 
-
-
-
-//This is the interruption routine
+// This is the interruption routine
 ISR(PCINT0_vect) {
-  //Input from optocoupler
-  //We make an AND with the pin state register, We verify if pin 8 is HIGH?
-  if(PINB & B00000001){
-    //If the last state was 0, then we have a state change
-    if(last_CH1_state == 0){
-      //We have detected a state change!
-      myDelay = 1;
-      last_CH1_state = 1;
+  // If pin D8 is high
+  if(PINB & 0x01){
+    // If previous state was low
+    if(prevStatus == 0){
+      // We have detected rising edge. Enabling phase control
+      enablePhaseControl = true;
+      // updating preStatus to current state i.e., high (1).
+      prevStatus = 1;
     }
   }
-  //If pin 8 is LOW and the last state was HIGH then we have a state change
-  else if(last_CH1_state == 1){
-    //We have detected a state change!
-    myDelay = 1;
-    //Store the current state into the last state for the next loop
-    last_CH1_state = 0;
+  // Else D8 is low and if previous state was high
+  else if(prevStatus == 1){
+    // We have detected a falling edge.
+    // updating preStatus to current state i.e., low (0).
+    prevStatus = 0;
   }
 }
